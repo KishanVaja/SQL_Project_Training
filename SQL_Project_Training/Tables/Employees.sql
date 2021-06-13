821f494bcdb0e7e57919c0b073a56a3c07d09d3e
Kishan Vaja <Kishan.Vaja@civica2.com>
2021-06-13 17:44:49 +0530

    Created Database

--- SQL_Project_Training/Tables/Employees.sql
+++ SQL_Project_Training/Tables/Employees.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Employees]
 (
 	[emp_no] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
-    [birth_date ] DATE NULL, 
+    [birth_date] DATE NULL, 
     [first_name] VARCHAR(14) NULL, 
     [last_name] VARCHAR(16) NULL, 
     [gender] CHAR(6) NULL, 
