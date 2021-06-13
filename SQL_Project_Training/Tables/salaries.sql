fb7bcec0a0721470d3940fe5241c3a2043428bb4
Kishan Vaja <Kishan.Vaja@civica2.com>
2021-06-13 15:57:07 +0530

    All Tables created.

---
+++ SQL_Project_Training/Tables/salaries.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Salaries]
+(
+	[emp_no] INT NOT NULL , 
+    [salary] INT NULL, 
+    [from_date] DATE NOT NULL, 
+    [to_date] DATE NULL, 
+    PRIMARY KEY ([from_date]),
+    CONSTRAINT FK_Emp_no FOREIGN KEY (emp_no)
+    REFERENCES Employees (emp_no)
+)
